725863ec81087d7ec22c553c768db11decfd6384
Sakshipatel0528 <85150493+Sakshipatel0528@users.noreply.github.com>
2021-06-03 17:43:56 +0530

    day1.php

---
+++ Day-1/demo.php
@@ -0,0 +1,14 @@
+<?php
+
+echo "welcome to php";
+echo "<h1>i am h1 in php</h1>";
+?>
+<html>
+    <body>
+        welcome to htmll code's
+        
+        <?php
+        echo "i am php inside html";
+        ?>
+    </body>
+</html>
