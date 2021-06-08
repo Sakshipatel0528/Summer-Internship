e7f5994cefa803595017c72267b88279e1f640f6
Sakshipatel0528 <85150493+Sakshipatel0528@users.noreply.github.com>
2021-06-08 11:46:23 +0530

    Add files via upload

---
+++ Day-8/3-table2.php
@@ -0,0 +1,27 @@
+<?php
+//DB connection
+$connection = mysqli_connect("localhost","root","","db_internship1");
+
+//Query
+$q = mysqli_query($connection,"select * from tb_user where is_delete=0") or die(mysqli_error($connection));
+
+echo "<table border='1'>";
+echo "<tr>";
+echo "<th>ID</th>";
+echo "<th>NAME</th>";
+echo "<th>GENDER</th>";
+echo "<th>MOBILE</th>";
+echo "<th>ACTION</th>";
+echo "</tr>";
+while($row = mysqli_fetch_array($q)){
+   echo "<tr>";
+   echo "<td>{$row['user_id']}</td>";
+   echo "<td>{$row['user_name']}</td>";
+   echo "<td>{$row['user_gender']}</td>";
+   echo "<td>{$row['user_mobile']}</td>";
+   echo "<td><a href='edit.php?id={$row['user_id']}'>Edit</a> | <a href='delete.php?deleteid={$row['user_id']}'>Delete</a></td>";
+   echo "</tr>";
+}
+echo "</table>";
+
+echo "<a href='insertrecord.php'>Add Record</a>";
